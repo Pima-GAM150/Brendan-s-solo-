2ab1477d37dbe5bf483beddae6987e0bbc2dc2ae
BrendanCraft <31329633+BrendanCraft@users.noreply.github.com>
2017-09-14 19:59:04 -0700

    Add files via upload

---
+++ Assets/controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+    public float speed = 10.0F;
+    // Use this for initialization
+    GameObject Person;
+
+    void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, translation, 0);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+}
